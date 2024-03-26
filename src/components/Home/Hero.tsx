ee623925fdbc68f05f8e85c6c3f0d3c8a45edb0e
tamani phiri <tamanigabriel0@gmail.com>
2024-03-26 22:04:57 +0200

    c

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       className="w-full bg-cover bg-center bg-no-repeat  text-white"
     >
       <div className="min-h-screen bg-black bg-opacity-40 flex gap-4 flex-col px-4 md:px-10 items-center justify-center text-center backdrop-blur-[2px]">
-        <h1 className="font-bold text-5xl"> Wheels Of Hope</h1>
+        <h1 className="font-bold text-5xl">Wheels Of Hope</h1>
         <p className="lg:max-w-xl md:text-xl text-lg text-gray-200">
           This organization is based on values of self help, self
           responsibility, democracy, equality and honest openess, social
