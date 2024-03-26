1b3ef9f3d16aeb2e215f795b900bacea40c0fd89
tamani phiri <tamanigabriel0@gmail.com>
2024-03-26 12:13:45 +0200

    c

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -6,6 +6,9 @@ export const objectives = [
     obj: "Support women's education through comprehensive adult literacy, vocational skills training, and accessible primary education.",
   },
   {
-    obj: " Promote gender equity through programs addressing social justice, gender-based violence, and encouraging women's participation.",
+    obj: "Promote gender equity through programs addressing social justice, gender-based violence, and encouraging women's participation.",
+  },
+  {
+    obj: "Empower women in leadership roles by providing mentorship, leadership training, and opportunities for career advancement.",
   },
 ];
