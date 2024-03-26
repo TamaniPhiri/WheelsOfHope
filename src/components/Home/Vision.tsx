752441e041aaf3d9c2624f853465d5caa51826d4
tamani phiri <tamanigabriel0@gmail.com>
2024-03-26 21:59:48 +0200

    c

--- src/components/Home/Vision.tsx
+++ src/components/Home/Vision.tsx
@@ -1,5 +1,28 @@
+import bg from "../../assets/vision.jpg";
+
 const Vision = () => {
-  return <div>vision</div>;
+  return (
+    <section className=" min-h-screen bg-[#ffcc99] bg-opacity-30 md:grid-cols-2 grid-cols-1 w-full px-4 md:px-12">
+      <div>
+        <img src={bg} alt="visionImg" loading="lazy" />
+      </div>
+      <div>
+        <h1>Vision</h1>
+        <h1>Envisioning Care, Dignity, and Inclusion</h1>
+        <div>dashes</div>
+        <p>
+          This vision statement embodies our commitment to creating a world
+          where individuals affected by gender-based violence (GBV), vulnerable
+          children (OVC), and the elderly are empowered to lead healthy,
+          dignified lives. We strive for a society where everyone enjoys health,
+          dignity, and inclusion, free from stigma and disease. Our vision is to
+          champion relentless support for all vulnerable groups and to ensure
+          that every person, regardless of age or circumstance, is valued and
+          treated with respect.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Vision;
