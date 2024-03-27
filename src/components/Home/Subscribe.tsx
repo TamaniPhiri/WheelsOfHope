c1e5cc9318df373cf36830e2e4f3e87b2bc766e9
tamani phiri <tamanigabriel0@gmail.com>
2024-03-27 22:49:59 +0200

    c

--- src/components/Home/Subscribe.tsx
+++ src/components/Home/Subscribe.tsx
@@ -1,8 +1,8 @@
 const Subscribe = () => {
   return (
-    <section className=" relative w-full items-center justify-center px-4 md:px-12">
-      <div className=" absolute w-full -top-4 bg-white p-4 rounded-lg shadow-xl">
-        hello
+    <section className=" relative w-full items-center justify-center px-4 md:px-12 min-h-[60vh]">
+      <div className=" absolute w-full -top-4 inset-x-0  px-4 md:px-12">
+        <div className="bg-white p-4 w-full flex shadow-xl">ge</div>
       </div>
     </section>
   );
