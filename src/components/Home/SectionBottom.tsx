1f06e2d69daae1004b023082c53e88c2af8883d4
tamani phiri <tamanigabriel0@gmail.com>
2024-03-27 22:35:03 +0200

    c

--- src/components/Home/SectionBottom.tsx
+++ src/components/Home/SectionBottom.tsx
@@ -4,9 +4,9 @@ const SectionBottom = () => {
   return (
     <section
       style={{ backgroundImage: `url(${bg})` }}
-      className=" min-h-[70vh] items-center justify-center text-center px-4 md:px-12 w-full bg-cover bg-center bg-no-repeat"
+      className=" min-h-[70vh] relative items-center justify-center text-center px-4 md:px-12 w-full bg-cover bg-center bg-no-repeat"
     >
-      help
+      <div className=" inset-0 bg-black absolute bg-opacity-70">fhe</div>
     </section>
   );
 };
