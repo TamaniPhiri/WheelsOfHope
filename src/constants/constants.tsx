04e21da73dc3544a875bc18bad2febe5e60d2b4e
tamani phiri <tamanigabriel0@gmail.com>
2024-03-27 21:53:56 +0200

    c

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,14 +1,18 @@
 export const objectives = [
   {
-    obj: "The Wheels of Hope GBV/OVCs Coalition values the protection of GBV cases, and children, particularly orphans and vulnerable children, from violence and exploitation. The Wheels of Hope GBV/OVCs Coalition is committed to preventing, detecting, and responding to violence against women, men and children, including sexual and gender-based violence, to ensure victims’ safety and well-being.",
+    title: "",
+    obj: "Committed to protecting GBV victims and vulnerable children, preventing violence, and ensuring victims’ safety and well-being.",
   },
   {
-    obj: "Empowerment and support are core values, with a focus on providing services to survivors of sexual and gender-based violence, including orphans and vulnerable children. The Wheels of Hope GBV/OVCs Coalition aims to empower young people to uphold universal human rights, combat gender-based violence, and foster a culture of respect and equality.",
+    obj: "Empowering survivors of GBV and vulnerable children, fostering respect, equality, and upholding human rights.",
   },
   {
-    obj: "The care and well-being of orphans and vulnerable children are central values, with a commitment to providing health care services and support to ensure their physical, emotional, and psychological well-being.",
+    obj: "Ensuring the care and well-being of vulnerable children, providing comprehensive support for their physical, emotional, and psychological needs.",
   },
   {
-    obj: "The Wheels of Hope GBV/OVCs Coalition values the reduction of vulnerability to HIV infection and gender-based violence through targeted interventions and support for GBV victims, orphans and vulnerable children. The organisation aims to improve the social and health outcomes of these vulnerable populations.",
+    obj: "Reducing vulnerability to HIV and GBV, enhancing social and health outcomes for victims through targeted interventions and support.",
+  },
+  {
+    obj: "Promoting gender equality and non-discrimination, addressing the unique needs of GBV victims and vulnerable children to reduce gender disparities and vulnerabilities.",
   },
 ];
