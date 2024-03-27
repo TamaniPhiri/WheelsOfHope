0c3486626bc499c1bae87453cb5a2edef8d3b9cb
tamani phiri <tamanigabriel0@gmail.com>
2024-03-27 22:18:16 +0200

    c

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { links } from "../constants/constants";
 
 const NavBar = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const location = useLocation();
   return (
     <nav
       className={`flex items-center max-w-7xl font-bold shadow-lg border-b top-0 z-50 backdrop-blur w-full justify-between px-4 md:px-12 py-6`}
@@ -16,50 +18,19 @@ const NavBar = () => {
           className=" object-cover w-16 md:w-[4.5rem] h-full hover:rotate-2 duration-200 delay-150"
         />
       </Link>
-      <li className="list-none gap-4 lg:flex hidden">
-        <Link
-          to={"/"}
-          className=" hover:text-[#ff6600] text-gray-700 duration-200"
-        >
-          HOME
-        </Link>
-        <Link
-          to={"/about"}
-          className=" hover:text-[#ff6600] text-gray-700 duration-200"
-        >
-          ABOUT
-        </Link>
-        <Link
-          to={"/services"}
-          className=" hover:text-[#ff6600] text-gray-700 duration-200"
-        >
-          RESOURCES
-        </Link>
-        <Link
-          to={"/blog"}
-          className=" hover:text-[#ff6600] text-gray-700 duration-200"
-        >
-          GET-INVOLVED
-        </Link>
-        <Link
-          to={"/services"}
-          className=" hover:text-[#ff6600] text-gray-700 duration-200"
-        >
-          SUPPORT
-        </Link>
-        <Link
-          to={"/services"}
-          className=" hover:text-[#ff6600] text-gray-700 duration-200"
-        >
-          NEWS
-        </Link>
-        <Link
-          to={"/services"}
-          className=" hover:text-[#ff6600] text-gray-700 duration-200"
-        >
-          CONTACT-US
-        </Link>
-      </li>
+      <div className="list-none gap-4 lg:flex hidden">
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            to={link.to}
+            className={`hover:text-[#ff6600] text-gray-700 duration-200 ${
+              location.pathname === link.to ? "text-[#ff6600]" : ""
+            }`}
+          >
+            {link.text}
+          </Link>
+        ))}
+      </div>
       <button
         onClick={() => setOpen(!open)}
         className="flex lg:hidden flex-col gap-3"
@@ -81,49 +52,18 @@ const NavBar = () => {
         } md:hidden top-[101%] bg-gray-200 border-t-0 max-w-7xl delay-150 duration-300 bg-opacity-[98%] backdrop-blur-md w-full inset-x-0`}
       >
         <div className=" text-2xl flex min-h-screen flex-col h-full w-full gap-4 items-center list-none">
-          <div className=" text-center flex flex-col mt-20 gap-2">
-            <Link
-              to={"/"}
-              className=" hover:text-[#ff6600] text-gray-700 duration-200"
-            >
-              HOME
-            </Link>
-            <Link
-              to={"/about"}
-              className=" hover:text-[#ff6600] text-gray-700 duration-200"
-            >
-              ABOUT
-            </Link>
-            <Link
-              to={"/services"}
-              className=" hover:text-[#ff6600] text-gray-700 duration-200"
-            >
-              RESOURCES
-            </Link>
-            <Link
-              to={"/blog"}
-              className=" hover:text-[#ff6600] text-gray-700 duration-200"
-            >
-              GET-INVOLVED
-            </Link>
-            <Link
-              to={"/services"}
-              className=" hover:text-[#ff6600] text-gray-700 duration-200"
-            >
-              SUPPORT
-            </Link>
-            <Link
-              to={"/services"}
-              className=" hover:text-[#ff6600] text-gray-700 duration-200"
-            >
-              NEWS
-            </Link>
-            <Link
-              to={"/services"}
-              className=" hover:text-[#ff6600] text-gray-700 duration-200"
-            >
-              CONTACT-US
-            </Link>
+          <div className="text-center flex flex-col mt-20 gap-2">
+            {links.map((link, index) => (
+              <Link
+                key={index}
+                to={link.to}
+                className={`hover:text-[#ff6600] text-gray-700 duration-200 ${
+                  location.pathname === link.to ? "text-[#ff6600]" : ""
+                }`}
+              >
+                {link.text}
+              </Link>
+            ))}
           </div>
         </div>
       </div>
