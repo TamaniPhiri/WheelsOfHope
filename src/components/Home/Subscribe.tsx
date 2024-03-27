496282b3a2010794e2c3fdb5185af2c37702867c
tamani phiri <tamanigabriel0@gmail.com>
2024-03-27 22:45:47 +0200

    c

---
+++ src/components/Home/Subscribe.tsx
@@ -0,0 +1,9 @@
+const Subscribe = () => {
+  return (
+    <section className=" relative w-full items-center justify-center px-4 md:px-12">
+      <div className=" absolute w-full top-4">hello</div>
+    </section>
+  );
+};
+
+export default Subscribe;
