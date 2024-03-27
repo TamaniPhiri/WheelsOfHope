4916ee024d658ddd9c3f4592ce7284d273e466dc
tamani phiri <tamanigabriel0@gmail.com>
2024-03-27 21:58:04 +0200

    c

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,18 +1,22 @@
 export const objectives = [
   {
-    title: "",
+    title: "Protection and Prevention",
     obj: "Committed to protecting GBV victims and vulnerable children, preventing violence, and ensuring victims’ safety and well-being.",
   },
   {
+    title: "Empowerment and Support",
     obj: "Empowering survivors of GBV and vulnerable children, fostering respect, equality, and upholding human rights.",
   },
   {
+    title: "Care and Well-being",
     obj: "Ensuring the care and well-being of vulnerable children, providing comprehensive support for their physical, emotional, and psychological needs.",
   },
   {
+    title: "Reduction of Vulnerability",
     obj: "Reducing vulnerability to HIV and GBV, enhancing social and health outcomes for victims through targeted interventions and support.",
   },
   {
+    title: "Gender Equality and Non-Discrimination",
     obj: "Promoting gender equality and non-discrimination, addressing the unique needs of GBV victims and vulnerable children to reduce gender disparities and vulnerabilities.",
   },
 ];
