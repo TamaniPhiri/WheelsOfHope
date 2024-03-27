74b361cda487a68120a5b3b570537977acafbada
tamani phiri <tamanigabriel0@gmail.com>
2024-03-27 22:36:04 +0200

    c

--- src/components/Home/SectionBottom.tsx
+++ src/components/Home/SectionBottom.tsx
@@ -6,7 +6,13 @@ const SectionBottom = () => {
       style={{ backgroundImage: `url(${bg})` }}
       className=" min-h-[70vh] relative items-center justify-center text-center px-4 md:px-12 w-full bg-cover bg-center bg-no-repeat"
     >
-      <div className=" inset-0 bg-black absolute bg-opacity-70">fhe</div>
+      <div className=" inset-0 bg-black absolute bg-opacity-70">
+        <h1>Thousands need a helping hand</h1>
+        <p>
+          Contribute on our charity work by your donation. Thanks for your heart
+          ♥.
+        </p>
+      </div>
     </section>
   );
 };
