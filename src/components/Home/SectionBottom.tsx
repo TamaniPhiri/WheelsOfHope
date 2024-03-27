fe91e0f184caaf10ee642d67e861d4902bf7bc95
tamani phiri <tamanigabriel0@gmail.com>
2024-03-27 22:32:41 +0200

    c

--- src/components/Home/SectionBottom.tsx
+++ src/components/Home/SectionBottom.tsx
@@ -1,5 +1,14 @@
+import bg from "../../assets/below.jpg";
+
 const SectionBottom = () => {
-  return <section>help</section>;
+  return (
+    <section
+      style={{ backgroundImage: `url(${bg})` }}
+      className=" min-h-[50vh] items-center justify-center text-center px-4 md:px-12"
+    >
+      help
+    </section>
+  );
 };
 
 export default SectionBottom;
