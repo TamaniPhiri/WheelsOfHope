52b836d69e9dc28de026ca0c75fff211652d06de
tamani phiri <tamanigabriel0@gmail.com>
2024-03-27 22:53:32 +0200

    c

--- src/components/Home/Subscribe.tsx
+++ src/components/Home/Subscribe.tsx
@@ -1,10 +1,12 @@
 const Subscribe = () => {
   return (
-    <section className=" relative w-full items-center justify-center px-4 md:px-12 min-h-[50vh]">
-      <div className=" absolute w-full -top-24 inset-x-0  px-4 md:px-12">
-        <div className="bg-white p-4 w-full flex shadow-xl rounded-xl">
-          <h1>Subscribe today</h1>
-          <p>Contribute on our charity work by your donation.</p>
+    <section className=" relative w-full items-center justify-center px-4 md:px-12 min-h-[30vh]">
+      <div className=" absolute w-full -top-24 inset-x-0 px-4 md:px-12">
+        <div className="bg-white p-4 flex-col w-full flex justify-center items-center shadow-xl rounded-xl">
+          <h1 className=" text-center">Subscribe today</h1>
+          <p className=" text-center">
+            Contribute on our charity work by your donation.
+          </p>
           <div>
             <input type="text" />
             <input type="text" />
